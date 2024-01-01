a9db7245b640a2c499f969f2de4ccc6531e153d3
tien.nh <hungtien408@gmail.com>
2024-01-01 16:48:51 +0700

    Setup axiosClient

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+import axiosClient from './api/axiosClient';
+
 console.log('Hello main.js');
+
+async function main() {
+  const response = await axiosClient.get('/posts');
+  console.log(response);
+}
+
+main();
