2f4a6dbc32a61580d987b9d8b3502237d2ed0fb8
tien.nh <hungtien408@gmail.com>
2024-01-08 22:51:18 +0700

    handle api errors

--- js/api/axiosClient.js
+++ js/api/axiosClient.js
@@ -31,8 +31,15 @@ axiosClient.interceptors.response.use(
     return response.data;
   },
   function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
+    if (!error.response) throw new Error('Network error. Please try again later.');
+
+    // redirect to login if not login
+    if (error.response.status === 401) {
+      // clear token, logout
+      window.location.assign('/login.html');
+      return;
+    }
+
     return Promise.reject(error);
   },
 );
--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@ import postApi from './api/postApi';
 // console.log('Hello main.js');
 
 async function main() {
-  const queryParams = {
-    _page: 1,
-    _limit: 5,
-  };
-  const data = await postApi.getAll(queryParams);
-  console.log(data);
+  try {
+    const queryParams = {
+      _page: 1,
+      _limit: 5,
+    };
+    const data = await postApi.getAll(queryParams);
+    console.log(data);
+  } catch (error) {
+    console.log('get all failed', error);
+    // show modal, toast
+  }
 }
 
 main();
